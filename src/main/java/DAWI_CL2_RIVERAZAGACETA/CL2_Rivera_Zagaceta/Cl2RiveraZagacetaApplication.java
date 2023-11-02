2d218216bb5ca3397e8578a735e1ce69593c3e90
Sebastian Rivera Zagaceta <rz_sebas@hotmail.com>
2023-11-01 23:23:01 -0500

    Inicio

---
+++ src/main/java/DAWI_CL2_RIVERAZAGACETA/CL2_Rivera_Zagaceta/Cl2RiveraZagacetaApplication.java
@@ -0,0 +1,13 @@
+package DAWI_CL2_RIVERAZAGACETA.CL2_Rivera_Zagaceta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2RiveraZagacetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2RiveraZagacetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DAWI_CL2_RIVERAZAGACETA/CL2_Rivera_Zagaceta/Cl2RiveraZagacetaApplicationTests.java
@@ -0,0 +1,13 @@
+package DAWI_CL2_RIVERAZAGACETA.CL2_Rivera_Zagaceta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2RiveraZagacetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
